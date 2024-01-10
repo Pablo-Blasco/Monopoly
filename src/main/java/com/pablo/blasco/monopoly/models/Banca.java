c6ca697a94dfd9caf71bc1700bd02df070d70756
Pablo Blasco <pabloblasco20@hotmail.com>
2024-01-10 17:00:37 +0100

    segunda
    segunda

---
+++ src/main/java/com/pablo/blasco/monopoly/models/Banca.java
@@ -0,0 +1,42 @@
+package com.pablo.blasco.monopoly.models;
+
+
+public class Banca {
+          private double dinero = 0.0f;
+          
+          private String causaHistorico = "";
+
+          private boolean enQuiebra =false; // En un futuro es un ArrayList
+          
+		public Banca(double dinero, String causaHistorico, boolean enQuiebra) {
+			this.dinero = dinero;
+			this.causaHistorico = causaHistorico;
+			this.enQuiebra = enQuiebra;
+		}
+
+		public boolean isEnQuiebra() {
+			return enQuiebra;
+		}
+
+		public void setEnQuiebra(boolean enQuiebra) {
+			this.enQuiebra = enQuiebra;
+		}
+
+		public double getDinero() {
+			return dinero;
+		}
+
+		public void setDinero(double dinero) {
+			this.dinero = dinero;
+		}
+
+		public String getCausaHistorico() {
+			return causaHistorico;
+		}
+
+		public void setCausaHistorico(String causaHistorico) {
+			this.causaHistorico = causaHistorico;
+		}
+		
+		
+}
---
+++ src/test/java/com/pablo/blasco/monopoly/models/JugadoresTes.java
@@ -0,0 +1,7 @@
+package com.pablo.blasco.monopoly.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JugadoresTes {
+
+}
